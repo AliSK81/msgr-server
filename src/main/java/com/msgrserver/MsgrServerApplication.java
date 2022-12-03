22c08fa516226af489b8d0355d2eb7bbd5a94ab0
Ali Ebrahimi <aebrahimi6739@gmail.com>
2022-12-03 12:59:33 +0330

    init project

---
+++ src/main/java/com/msgrserver/MsgrServerApplication.java
@@ -0,0 +1,13 @@
+package com.msgrserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsgrServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsgrServerApplication.class);
+    }
+
+}
---
+++ src/test/java/com/msgrserver/MsgrServerApplicationTests.java
@@ -0,0 +1,11 @@
+package com.msgrserver;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
+class MsgrServerApplicationTests {
+
+}
