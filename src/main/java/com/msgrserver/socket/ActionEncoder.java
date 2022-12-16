a52cebd0c062960122596bbc6c57564d68761a1e
Mahsa Aghaie <mahsa.mae14@gmail.com>
2022-12-16 23:46:16 +0330

    add encrypt decrypt message

--- src/main/java/com/msgrserver/socket/ActionEncoder.java
+++ src/main/java/com/msgrserver/socket/ActionEncoder.java
@@ -5,16 +5,28 @@ import com.msgrserver.action.Action;
 import com.msgrserver.util.Mapper;
 import jakarta.websocket.Encoder;
 import jakarta.websocket.EndpointConfig;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import java.io.UnsupportedEncodingException;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
 
 public class ActionEncoder implements Encoder.Text<Action> {
 
     @Override
     public String encode(Action action) {
         try {
-            // todo encrypt json message
-
-            return Mapper.toJson(action);
-        } catch (JsonProcessingException e) {
+            String jsonMessage = Mapper.toJson(action);
+            EncryptDecrypt encryptDecrypt = new EncryptDecrypt();
+            return encryptDecrypt.encrypt(jsonMessage);
+        } catch (JsonProcessingException | UnsupportedEncodingException | NoSuchPaddingException |
+                 NoSuchAlgorithmException | InvalidAlgorithmParameterException | InvalidKeyException |
+                 BadPaddingException | IllegalBlockSizeException e) {
             throw new RuntimeException(e);
         }
     }
---
+++ src/test/java/com/msgrserver/socket/EncryptDecryptTest.java
@@ -0,0 +1,27 @@
+package com.msgrserver.socket;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+@SpringBootTest
+class EncryptDecryptTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void encrypt() {
+    }
+
+    @Test
+    void decrypt() {
+    }
+}
