3947c081c777ca4d9e93effce9f10c1ae39898e9
sorour rokni <soroorrokni@gmail.com>
2022-12-27 23:54:41 +0330

    fix conflict

--- src/main/java/com/msgrserver/handler/ChatHandlerImpl.java
+++ src/main/java/com/msgrserver/handler/ChatHandlerImpl.java
@@ -1,12 +1,10 @@
 package com.msgrserver.handler;
 
-import com.msgrserver.service.ChatService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
 public class ChatHandlerImpl {
-    private final ChatService chatService;
 
 }
